b1f926c6d7b5d9218025357f1250bd3c22df8f29
n8hadden <n8hadden888@gmail.com>
2024-01-31 13:27:58 -0700

    Added postgres to index.js and got rid of mySQL

--- server/index.js
+++ server/index.js
@@ -3,3 +3,17 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+
+// Setup PostgreSQL Connection
+import { Client } from 'pg';
+const client = new Client();
+await client.connect();
+
+// Running postgres queries with error handling
+try {
+   
+} catch (err) {
+    console.error(err);
+} finally {
+    await client.end();
+}
