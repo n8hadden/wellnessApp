238ecdf248603490f88aa4f492075bc4041d3624
n8hadden <n8hadden888@gmail.com>
2024-02-01 12:26:02 -0700

    Added connection

--- server/index.js
+++ server/index.js
@@ -9,4 +9,15 @@ require("dotenv").config();
 //connect to database
 new database().connect()
 
+// Setup PostgreSQL Connection
+import { Client } from 'pg';
+const client = new Client({
+	host: 'host',
+	port: 5334,
+	database: 'database-name',
+	user: 'database-user',
+	password: 'secretpasswordooohhhhhhfun'
+});
+await client.connect();
+
 // Running postgres queries with error handling
