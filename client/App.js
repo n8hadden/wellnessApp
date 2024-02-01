527f9869ded8b9cf2b1f71f1d1f0dead2943711b
Daniel Chiquete <dchiquete2019@gmail.com>
2024-02-01 14:18:14 -0700

    home+navigation

--- client/App.js
+++ client/App.js
@@ -1,19 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Home from './pages/Home';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+// Screens
+import HomePage from './pages/Home';
+import ChatPage from './pages/Chat';
+
 export default function App() {
+
+  //Screen names
+  const homeName = "Home";
+  const chatName = "Chat";
+
+  const Tab = createBottomTabNavigator();
+
   return (
-    <View style={styles.container}>
-      <Home />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName={homeName}
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            let rn = route.name;
+
+            if (rn === homeName) {
+              iconName = focused ? 'home' : 'home-outline';
+            } else if (rn === chatName) {
+              iconName = focused ? 'chatbubbles-sharp' : 'chatbubbles-outline';
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size*1.5} color={color} />;
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'grey',
+          tabBarLabelStyle: { paddingBottom: 0, fontSize: 15 }, 
+          tabBarStyle: { padding: 10, height: 100, backgroundColor: 'lightgrey',  }
+        })}
+      >
+
+        <Tab.Screen name={homeName} component={HomePage} />
+        <Tab.Screen name={chatName} component={ChatPage} />
+        
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- client/pages/Chat.js
+++ client/pages/Chat.js
@@ -1,18 +1,14 @@
-// npx expo start --tunnel
-
 import { StatusBar } from 'expo-status-bar';
 import { Dimensions, StyleSheet, Text, View, Image, ScrollView  } from 'react-native';
 import React from 'react';
 import { styles } from '../styles/HomeStyles'
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-export default function Home() {
-    const windowWidth = Dimensions.get('window').width;
-    const windowHeight = Dimensions.get('window').height;
+export default function Chat() {
 
     return (
         <ScrollView contentContainerStyle={styles.container}>
-            <Text style={styles.title}>Hello World!</Text>
+            <Text style={styles.title}>Chat Page</Text>
             <StatusBar style="auto" />
         </ScrollView>
     );
