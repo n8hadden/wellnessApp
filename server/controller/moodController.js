d30bfd2cfedb876d5e289e9c8af7249d2b81ae92
Ethan-Heimer <111470266+Ethan-Heimer@users.noreply.github.com>
2024-02-22 12:05:17 -0700

    Refactor Mood Algorithm

--- server/controller/moodController.js
+++ server/controller/moodController.js
@@ -1,56 +1,7 @@
 const model = require("../models/moodModels");
 
-// Used for testing the algorithm
-const test_data = [
-  { moodName: "Depressed", moodScore: 1, moodType: "Sad" },
-  { moodName: "Happy", moodScore: 5, moodType: "Happy" },
-  { moodName: "Mad", moodScore: 3, moodType: "Mad" },
-  { moodName: "Sad", moodScore: 2, moodType: "Sad" },
-  { moodName: "Joyful", moodScore: 4, moodType: "Joyful" },
-  { moodName: "Weird", moodScore: 3, moodType: "Weird" },
-  { moodName: "Calm", moodScore: 4, moodType: "Calm" },
-  { moodName: "Depressed", moodScore: 5, moodType: "Depressed" },
-  { moodName: "Loved", moodScore: 5, moodType: "Loved" },
-  { moodName: "Depressed", moodScore: 1, moodType: "Sad" },
-  { moodName: "Happy", moodScore: 4, moodType: "Happy" },
-  { moodName: "Mad", oodScore: 2, moodType: "Mad" },
-  { moodName: "Sad", moodScore: 1, moodType: "Sad" },
-  { moodName: "Joyful", moodScore: 5, moodType: "Joyful" },
-  { moodName: "Weird", moodScore: 3, moodType: "Weird" },
-  { moodName: "Calm", moodScore: 4, moodType: "Calm" },
-  { moodName: "Loved", moodScore: 5, moodType: "Loved" },
-];
-
-/**
-  * Algorithm to determine the highest rated mood
-  * @param {[{moodName: string, moodScore: number, moodType: string}]} data
-  * @returns {{moodName: string, moodScore: number, moodType: string}} mood 
-**/
-function getHighestRatedMood(data) {
-  // Priority queue
-  // Lower index is higher priority
-  const pque = ["Depressed", "Happy", "Mad", "Sad", "Joyful", "Weird", "Calm", "Loved"];
-
-  // Currently highest rated mood
-  let cur = data[0];
-  for (let i = 1; i < data.length; i++) {
-    const mood = data[i];
-    // Priority Queue Value (index: lowest)
-    const index = pque.indexOf(mood.moodType);
-    // Value is in the priority queue
-    if (index !== -1) {
-      if (mood.moodScore > cur.moodScore) {
-        cur = mood;
-      } else if ((mood.moodScore === cur.moodScore) && index < pque.indexOf(cur.moodType)) {
-        cur = mood;
-      }
-    }
-  }
-  return cur;
-}
-
 async function getMoodInfo(req, res) {
-  // const { data } = req.body;
+// const { data } = req.body;
   const data = test_data
 
   // Code to determine the highest rated score
@@ -64,3 +15,30 @@ async function getMoodInfo(req, res) {
     res.status(500).json({ error: "Internal Server Error" });
   }
 }
+
+function getHighestRatedMood(data) {
+  // Priority queue
+  // Lower index is higher priority
+  const pque = ["Depressed", "Happy", "Mad", "Sad", "Joyful", "Weird", "Calm", "Loved"];
+
+  // Currently highest rated mood
+  let cur = data[0];
+  for (let i = 1; i < data.length; i++) {
+    const mood = data[i];
+    // Priority Queue Value (index: lowest)
+    const index = pque.indexOf(mood.moodType);
+    // Value is in the priority queue
+    
+    if (index === -1) 
+     continue;
+     
+    if (mood.moodScore > cur.moodScore) {
+      cur = mood;
+    } else if ((mood.moodScore === cur.moodScore) && index < pque.indexOf(cur.moodType)) {
+      cur = mood;
+    }
+
+  }
+  return cur;
+}
+
