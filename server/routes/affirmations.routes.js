42d2dd71f1055618ea85345ef56ebc2db143a2fa
Ethan-Heimer <111470266+Ethan-Heimer@users.noreply.github.com>
2024-02-26 12:20:07 -0700

    Fixed Affirmations

--- server/routes/affirmations.routes.js
+++ server/routes/affirmations.routes.js
@@ -8,7 +8,7 @@ const router = express.Router();
 const controller = require("../controller/affirmController");
 
 // Defining a GET route '/get' which will be handled by the getAffirmation function from the affirmController module
-router.get('/get', controller.getAffirmation);
+router.post('/get', controller.getAffirmation);
 
 // Exporting the router object so it can be used in other parts of the application
 module.exports = router;
