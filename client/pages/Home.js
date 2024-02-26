419939f4d7f4861905627ee1d5a1eb420ef36f7b
Daniel Chiquete <dchiquete2019@gmail.com>
2024-02-26 13:27:45 -0700

    updated

--- client/pages/Home.js
+++ client/pages/Home.js
@@ -1,9 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import { Dimensions, StyleSheet, Text, View, Image, ScrollView, TouchableOpacity } from 'react-native';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Alert } from 'react-native';
 import { styles } from '../styles/HomeStyles'
 import Ionicons from 'react-native-vector-icons/Ionicons';
-
 
 // components
 import Btn from '../components/HomeBtn';
@@ -15,18 +15,53 @@ const testName = 'Daniel';
 
 export default function Page({route}) {
 
+    const [affirmation, setAffirmation] = useState(null);
+
     const navigation = useNavigation();
+
+    // https://wellness/affermation/get
+
+    useEffect(() => {
+        console.log(affirmation);
+    }, [affirmation]);
+
+    const handleaffirmation = async () => {
+        try {
+            // Make an HTTP request to the /user/login endpoint
+            const response = await fetch('https://wellness-server.onrender.com/affirmation/get', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    user_id: 1, // user_id: user_id,
+                }),
+            });
+            if (response.ok) {
+                const userData = await response.json();
+                setAffirmation(userData.affirmation);
+                console.log(userData);
+            } else {
+                Alert.alert('Error', 'Invalid username or password. Please try again.');
+            }
+        } catch (error) {
+          console.error('Error:', error);
+          Alert.alert('Error', 'An error occurred. Please try again later.');
+        }
+    };
+
+    handleaffirmation();
     
     return (
         <ScrollView contentContainerStyle={styles.container}>
             <View style={styles.introContainer}>
                 <Text style={styles.introText}>Goodmorning, {testName}</Text>
                 <Text style={styles.introText}>Date, Time</Text>
-                <Text style={styles.introText}>Weather</Text>
+                <Text style={styles.introText}>{affirmation}</Text>
                 <Image 
                     style={styles.introImage}
                 />
-                <Text style={styles.introText}>Message</Text>
+                <Text style={styles.introText}>Daily Affirmation</Text>
             </View>
 
             <TouchableOpacity 
