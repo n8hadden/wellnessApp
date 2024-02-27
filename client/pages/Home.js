2d361d955285944931899211fa9fb0967f304405
Daniel Chiquete <dchiquete2019@gmail.com>
2024-02-26 20:47:38 -0700

    updated home styling

--- client/pages/Home.js
+++ client/pages/Home.js
@@ -4,19 +4,54 @@ import React, { useState, useEffect } from 'react';
 import { Alert } from 'react-native';
 import { styles } from '../styles/HomeStyles'
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
 
 // components
 import Btn from '../components/HomeBtn';
 
 import { useNavigation } from '@react-navigation/native';
+import { CardStyleInterpolators } from '@react-navigation/stack';
+import { color } from 'react-native-elements/dist/helpers';
 
 const windowWidth = Dimensions.get('window').width;
-const testName = 'Daniel';
+const user_name = 'Daniel';
 
 export default function Page({ route }) {
-    const [affirmation, setAffirmation] = useState(null);
 
+    const [weatherIcon, setWeatherIcon] = useState(null);
+    const [currentDateTime, setCurrentDateTime] = useState(new Date());
+    const [affirmation, setAffirmation] = useState(null);
     const navigation = useNavigation();
+
+    const currentDate = new Date();
+    const monthNames = [
+        "January", "February", "March",
+        "April", "May", "June", "July",
+        "August", "September", "October",
+        "November", "December"
+    ];
+
+    const weatherNames = [
+        "weather-cloudy", "weather-fog", "weather-hail", 
+        "weather-lightning", "weather-lightning-rainy", "weather-night", 
+        "weather-night-partly-cloudy", "weather-partly-cloudy", "weather-partly-lightning", 
+        "weather-partly-rainy", "weather-partly-snowy", "weather-partly-snowy-rainy",
+        "weather-pouring", "weather-rainy", "weather-snowy",
+        "weather-snowy-heavy", "weather-snowy-rainy", "weather-sunny", 
+        "weather-sunset", "weather-sunset-down", "weather-sunset-up", 
+        "weather-windy"
+    ]
+  
+    const formattedDate = `${monthNames[currentDate.getMonth()]} ${currentDate.getDate()}, ${currentDate.getFullYear()}`;  
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+        setCurrentDateTime(new Date());
+        }, 1000); // Update every second
+
+        return () => clearInterval(interval); // Clean up interval on unmount
+    }, []);
 
     useEffect(() => {
         console.log(affirmation);
@@ -37,7 +72,7 @@ export default function Page({ route }) {
             if (response.ok) {
                 const userData = await response.json();
                 setAffirmation(userData.affirmation);
-                console.log(userData);
+                // console.log(userData);
             } else {
                 Alert.alert('Error', 'Invalid username or password. Please try again.');
             }
@@ -52,20 +87,27 @@ export default function Page({ route }) {
     return (
         <ScrollView contentContainerStyle={styles.container}>
             <View style={styles.introContainer}>
-                <Text style={styles.introText}>Goodmorning, {testName}</Text>
-                <Text style={styles.introText}>Date, Time</Text>
-                <Text style={styles.introText}>{affirmation}</Text>
-                <Image 
-                    style={styles.introImage}
-                />
-                <Text style={styles.introText}>Daily Affirmation</Text>
+                <Text style={styles.introText}>Goodmorning, {user_name}</Text>
+                <Text style={styles.introText}>{/* Date, Weather */}{formattedDate}</Text>
+                <View style={styles.introImage}>
+                    <MaterialCommunityIcons 
+                        name={weatherNames[0]}
+                        size={120} 
+                        color="white" 
+                    />
+                </View>
             </View>
 
             <TouchableOpacity 
                 onPress={() => {  }}
             >
                 <View style={styles.dailyaffirm}>
-                    <Text>Daily Affrimation</Text>
+                    <Text style={{
+                        fontSize: 23,
+                        fontWeight: 500,
+                        color: 'white',
+                        textAlign: 'center',
+                    }}>Daily Affirmation: {affirmation}</Text>
                 </View>
             </TouchableOpacity>
 
