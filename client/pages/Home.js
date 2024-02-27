30ecac67affcf19c3d554121eebbb1cebb522b42
Daniel Chiquete <dchiquete2019@gmail.com>
2024-02-27 14:25:38 -0700

    working branch no errors

--- client/pages/Home.js
+++ client/pages/Home.js
@@ -38,33 +38,31 @@ export default function Page({ route }) {
         console.log(id, " id");
         if(id == undefined)
             return;
-        // /* ERROR: This is not getting the user information */
-        // try {
-        //     fetch('https://wellness-server.onrender.com/user/getUserById', {
-        //         method: 'POST',
-        //         headers: {
-        //             'Content-Type': 'application/json',
-        //         },
-        //         body: JSON.stringify({
-        //             _id: "1",
-        //         }),
-        //     })
-        //     .then(res => res.json())
-        //     .then(async res => {
-        //         const user = res;
-        //         setUserName(user.username);
-        //         console.log("username: ");
-        //         console.log(user);
-        //     })
-        //     .catch(err => {
-        //         console.log("the error")
-        //         console.error(err)
-        //     });
-        // } catch (error) {
-        //     console.error('Error:', error);
-        //     Alert.alert('Error', 'An error occurred. Please try again later.');
-        // }
-        // /* End of ERROR */
+        try {
+            fetch('https://wellness-server.onrender.com/user/getUserById', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    _id: "1",
+                }),
+            })
+            .then(res => res.json())
+            .then(async res => {
+                const user = res;
+                setUserName(user.username);
+                console.log("username: ");
+                console.log(user);
+            })
+            .catch(err => {
+                console.log("the error")
+                console.error(err)
+            });
+        } catch (error) {
+            console.error('Error:', error);
+            Alert.alert('Error', 'An error occurred. Please try again later.');
+        }
         try {
             fetch('https://wellness-server.onrender.com/affirmation/get', {
                 method: 'POST',
@@ -128,7 +126,7 @@ export default function Page({ route }) {
             </TouchableOpacity>
 
             <Btn 
-                onPress={() => { navigation.navigate('Home'); }}
+                onPress={() => { navigation.navigate('Home', {screen: 'MoodCalendarScreen'}); }}
                 iconImg="calendar"
                 iconColor="#ffffff"
                 text="Mood Calender"
